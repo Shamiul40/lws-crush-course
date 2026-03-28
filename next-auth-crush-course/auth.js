bb98496a5f99c2fb6adc7e91aaae3dd93381df60
Shamiul40 <shamiul4040@gmail.com>
2026-03-28 22:31:50 +0600

    title change

--- next-auth-crush-course/auth.js
+++ next-auth-crush-course/auth.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 
-export const {} = NextAuth({
+export const {signIn} = NextAuth({
        
     GoogleProvider
     })
