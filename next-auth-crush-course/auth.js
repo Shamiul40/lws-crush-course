3cfe590424f5210d8437424aebf77c32bded2e47
Shamiul40 <shamiul4040@gmail.com>
2026-03-31 22:00:19 +0600

    nextauth route.js create

--- next-auth-crush-course/auth.js
+++ next-auth-crush-course/auth.js
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
-
-export const {} = NextAuth({
-    
-    providers : [
-        GoogleProvider({
-        clientId : process.env.GOOGLE_CLIENT_ID,
-        clientSecret : process.env.GOOGLE_CLIENT_SECRET
-    })
-    ]
-    
-})
+export const { 
+    handlers : {
+        GET,POST
+    }
+    auth, signIn, signOut } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
