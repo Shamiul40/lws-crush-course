832af5a19f36621068104bb43b0cca7f1e4bd7ea
Shamiul40 <shamiul4040@gmail.com>
2026-04-01 22:55:39 +0600

    get post

--- next-auth-crush-course/auth.js
+++ next-auth-crush-course/auth.js
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const { 
-    handlers : {
-        GET,POST
-    }
-    auth, signIn, signOut } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
